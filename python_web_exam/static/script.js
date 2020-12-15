cc786ff1264990528c242c0f3f8cc35a16c9a31c
gbb <g.borisov1988@gmail.com>
2020-12-15 23:10:45 +0200

    minor css changes

--- python_web_exam/static/script.js
+++ python_web_exam/static/script.js
@@ -14,8 +14,6 @@ if(document.querySelector('.bg-modal')){
     })
 }
 
-
-
 document.querySelector('.mobile-menu').addEventListener('click', () => {
     document.querySelector('.bar-two').classList.toggle('rotated')
     document.querySelector('.bar-one').classList.toggle('rotated')
@@ -23,34 +21,34 @@ document.querySelector('.mobile-menu').addEventListener('click', () => {
     document.querySelector('.menu-wrapper').classList.toggle('show')
 })
 
-const nav = document.querySelector('.menu-container')
-const navTop = nav.offsetTop
+// const nav = document.querySelector('.menu-container')
+// const navTop = nav.offsetTop
+// debounce = (func, wait) => {
+// 	let timeout
+// 	return function() {
+//         let context = this, 
+//             args = arguments
+// 		let later = function() {
+// 			timeout = null
+// 		}
+// 		let callNow = !timeout
+// 		clearTimeout(timeout)
+// 		timeout = setTimeout(later, wait)
+// 		if (callNow) {
+//             func.apply(context, args)
+//         } 
+// 	}
+// }
+// fixedNav = debounce( () => {
+//     console.log(window.scrollY)
+//     console.log(nav.offsetTop)
+//     if (window.scrollY >= navTop){
+//         nav.classList.add('fixed')
+//         document.querySelector('main').classList.add('fixed-active')
+//     } else {
+//         nav.classList.remove('fixed')
+//         document.querySelector('main').classList.remove('fixed-active')
+//     }
+// }, 25)
 
-debounce = (func, wait) => {
-	let timeout
-	return function() {
-        let context = this, 
-            args = arguments
-		let later = function() {
-			timeout = null
-		}
-		let callNow = !timeout
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		if (callNow) {
-            func.apply(context, args)
-        } 
-	}
-}
-console.log(window.scrollY)
-fixedNav = debounce( () => {
-    if (window.scrollY >= navTop){
-        nav.classList.add('fixed')
-        document.querySelector('main').classList.add('fixed-active')
-    } else {
-        nav.classList.remove('fixed')
-        document.querySelector('main').classList.remove('fixed-active')
-    }
-}, 25)
-
-window.addEventListener('scroll', fixedNav)
+// window.addEventListener('scroll', fixedNav)
--- python_web_exam/views/index.py
+++ python_web_exam/views/index.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from python_web_exam.models import Posts
 
 def index(request):
-    data = {
-        'title': 'Home'
-    }
-    return render(request, 'index.html', data)
+    if request.method == 'GET':
+        if request.user == 'AnonymousUser':
+            posts = Posts.objects.all().order_by('-created')[:4]
+            data = {
+                'title': 'Home',
+                'posts':posts
+            }
+            return render(request, 'index.html', data)
+        else:
+            posts = Posts.objects.all().order_by('-created')[:2]
+            user_posts = Posts.objects.all().order_by('-created').filter(author=request.user)[:2]
+            data = {
+                'title': 'Home',
+                'posts':posts
+            }
+            return render(request, 'index.html', data)
+            
